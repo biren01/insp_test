7b50159dd44cef00ebab00e942905dccae999650
biren01 <raibiren@gmail.com>
2025-12-27 19:31:35 +0000

    Questions added

---
+++ src/components/QUESTIONS.js
@@ -0,0 +1,101 @@
+export const QUESTIONS = [
+    {
+        id: 1,
+        Question: "What would be the common reason for a local authority requiring  a periodic inspection and testing process?",
+         options: [
+            {id: 'a', text: "To confirm the installation is defective or has not detereiorated over time"},
+            {id: 'b', text: "To confirm the installation complies with the design or construction parts of BS 7671"},
+            {id: 'c', text: "To confirm the client is satisfied with all the work carried out"},
+            {id: 'd', text: "To confirm that contractual elements have been undertaken to ISO 9001"},
+        ],
+        correctAnswer: 'a',
+        explanation:"It is the local authority, such as district council, who issues licences and to ensure that building is safe and suitable for that use."
+    },
+
+    {
+        id: 2,
+        Question: "Which statutory document relates to the periodic inspection and testing process?",
+            options: [
+            {id: 'a', text: "BS 7671 IEE Wiring Regulations"},
+            {id: 'b', text: "HSE GS38 Test Instruments and Leads"},
+            {id: 'c', text: "Electricity at Work Regulations "},
+            {id: 'd', text: "IET GN3 Inspection and Testing"},
+        ],
+        correctAnswer: 'c',
+        explanation: " ",
+    {
+        id: 3,
+        Question: "What factor is a major consideration when an inspector is setting inspection and testing samples?", 
+            options: [
+            {id: 'a', text: "normal  working hours"},
+            {id: 'b', text: "Quality of record kept"},
+            {id: 'c', text: "Site access restrictions"},
+            {id: 'd', text: "Likely weather conditions"},
+        ],
+        correctAnswer: 'b',
+        explanation: "",
+    },
+
+    {
+        id: 4,
+        Question: "What factor could  have an impact on the limitations agreed with a client?",       
+            options: [
+            {id: 'a', text: "The sections of the building client occuipies"},
+            {id: 'b', text: "The ability to access rooms at particular times"},
+            {id: 'c', text: "The number of distribution circuits present"},
+            {id: 'd', text: "The loss of supply to an essential data system"},
+        ],
+        correctAnswer: 'd',
+        explanation: "",
+    },
+    {
+        id: 5,
+        Question: "What is a major responsibility of and inspector, based on the overall condition of an installation, following a periodic inspection?",
+            options: [
+            {id: 'a', text: "Setting the period to the next test date"},
+            {id: 'b', text: "Ensuring all minor works are completed"},
+            {id: 'c', text: "Issuing Certification to the local authority"},
+            {id: 'd', text: "Sending a general report to the DNO"},
+        ],
+        correctAnswer: 'a',
+        explanation: "",
+    },
+    {
+        id: 6,
+        Question: "What does the Electricity at Work Regulations state, with regard to, working on or near live parts?",
+            options: [
+            {id: 'a', text: "It is permitted  if the building is occupied during the works"},
+            {id: 'b', text: "Suitable precautions must be taken to prevent injury"},
+            {id: 'c', text: "Under no circumstances should this be undertaken"},
+            {id: 'd', text: "Those undertaking the work shall have notified the HSE in advance"},,
+        ],
+        correctAnswer: 'b',
+        explanation: "",
+    },
+    {
+        id: 7,
+        Question: " At large installation is being inspected by four persons and the supply has been isolated. How should the means of isolation be secured?",
+            options: [
+            {id: 'a', text: "By using four notices on the installation main switch."},
+            {id: 'b', text: "By using a device that accepts one lock having four keys."},
+            {id: 'c', text: "By using four different colours of tape over the main switch."},
+            {id: 'd', text: "By using a device that accepts one lock having four unique keys."},,
+        ],
+        correctAnswer: 'd',
+        explanation: "",
+    },
+
+    {
+        id:8 ,
+        Question: "What is the minimum level of IP protection, for basic protection, when inspecting the accessible horizontal top surface of a distribution board ?",
+            options: [
+            {id: 'a', text: "IP2X."},
+            {id: 'b', text: "IP4X."},
+            {id: 'c', text: "IPX2"},
+            {id: 'd', text: "IPX4."},,
+        ],
+        correctAnswer: 'b',
+    }, 
+
+    
+];
